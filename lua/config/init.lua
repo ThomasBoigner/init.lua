131411b4175f5bfe4cee6cb3802938574bcf47e0
Thomas Boigner <thomasboigner@outlook.com>
2023-09-21 22:20:59 +0200

    Add plugins for html development

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -47,5 +47,14 @@ require("lazy").setup(
             {'hrsh7th/cmp-nvim-lsp'},
             {'L3MON4D3/LuaSnip'},
         }
+    },
+    {
+        "m4xshen/autoclose.nvim"
+    },
+    {
+        "windwp/nvim-ts-autotag"
+    },
+    {
+        "tpope/vim-surround"
     }
 })
