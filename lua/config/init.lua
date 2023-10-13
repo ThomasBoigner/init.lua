b2da2d46dbbe3f6719555c6e2db09f2bfbbe8c0f
Thomas Boigner <thomasboigner@outlook.com>
2023-10-13 21:27:20 +0200

    Add Neotree plugin

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -65,5 +65,14 @@ require("lazy").setup(
             "antoinemadec/FixCursorHold.nvim",
             "marilari88/neotest-vitest"
         }
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        }
     }
 })
