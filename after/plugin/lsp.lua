eaf26f99ffadca05bbdd65d55610ea051d925db9
Thomas Boigner <thomasboigner@outlook.com>
2024-05-12 00:23:33 +0200

    Fix lsp for vue files

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -28,10 +28,41 @@ cmp.setup({
 
 mason.setup({})
 masonLspconfig.setup({
+    ensure_installed = {'tsserver', 'cssls', 'emmet_language_server', 'eslint', 'html', 'lua_ls', 'marksman', 'stylelint_lsp', 'volar'},
     handlers = {
         function(server_name)
-        require('lspconfig')[server_name].setup({})
+            require('lspconfig')[server_name].setup({})
         end,
+        volar = function()
+            require('lspconfig').volar.setup({})
+        end,
+        tsserver = function()
+            local vue_typescript_plugin = require('mason-registry')
+                .get_package('vue-language-server')
+                :get_install_path()
+                .. '/node_modules/@vue/language-server'
+                .. '/node_modules/@vue/typescript-plugin'
+
+            require('lspconfig').tsserver.setup({
+            init_options = {
+                plugins = {
+                    {
+                        name = "@vue/typescript-plugin",
+                        location = vue_typescript_plugin,
+                        languages = {'javascript', 'typescript', 'vue'}
+                    },
+                }
+            },
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'vue',
+        },
+      })
+    end,
     },
-    ensure_installed = {'tsserver', 'cssls', 'emmet_language_server', 'eslint', 'html', 'lua_ls', 'marksman', 'stylelint_lsp', 'volar'}
 })
