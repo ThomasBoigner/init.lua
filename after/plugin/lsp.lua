7917c815608f9cf7aa15a89ba4a56bb89d88ebba
Thomas Boigner <thomasboigner@outlook.com>
2024-05-11 23:36:09 +0200

    Update lsp-zero to version 3.x

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,23 +1,37 @@
 local lsp = require('lsp-zero')
-
-lsp.preset('recommended')
-
-lsp.nvim_workspace()
-
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-z>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+local mason = require('mason')
+local masonLspconfig = require('mason-lspconfig')
+local luasnip = require('luasnip')
+
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({buffer = bufnr})
+end)
+
+cmp.setup({
+    sources = {
+        {name = 'nvim_lsp'},
+    },
+    mapping = {
+        ['<C-z>'] = cmp.mapping.confirm({select = false, behavior = cmp.ConfirmBehavior.Replace}),
+        ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+        ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+mason.setup({})
+masonLspconfig.setup({
+    handlers = {
+        function(server_name)
+        require('lspconfig')[server_name].setup({})
+        end,
+    },
+    ensure_installed = {'tsserver', 'cssls', 'emmet_language_server', 'eslint', 'html', 'lua_ls', 'marksman', 'stylelint_lsp', 'volar'}
 })
-
-lsp.setup()
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -36,16 +36,15 @@ require("lazy").setup(
     },
     {
         'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        branch = 'v3.x',
         dependencies = {
             {'neovim/nvim-lspconfig'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/nvim-cmp'},
+            {'L3MON4D3/LuaSnip'},
+
             {'williamboman/mason.nvim'},
             {'williamboman/mason-lspconfig.nvim'},
-
-            --Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'L3MON4D3/LuaSnip'},
         }
     },
     {
